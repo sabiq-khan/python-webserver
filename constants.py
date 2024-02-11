2b59798e0883c97cfb07a504d82ce5e7b226e6df
Sabiq Khan <sabiq.am.khan@gmail.com>
2024-02-11 10:33:14 -0800

    Fixed help message and improved arg validation

--- constants.py
+++ constants.py
@@ -19,13 +19,11 @@ Usage: ./webserver.py [--hostname/-n HOSTNAME] [--port/-p PORT_NUMBER] [--help/-
 Starts an HTTP server. If no arguments are provided, the server listens on `localhost:8080`.
 
 Options:
-	--hostname/-n	String representing the host name or IP address of the network interface that the server will listen on.
-                    Default value is `localhost`.
+	--hostname/-n\tString representing the host name or IP address of the network interface that the server will listen on.\n\t\t\tDefault value is `localhost`.
 
-	--port/-p		Integer between 0 and 65535 representing the TCP port that the server will listen on.
-                    Default value is 8080.
+	--port/-p       Integer between 0 and 65535 representing the TCP port that the server will listen on.\n\t\t\tDefault value is 8080.
 
-	--help/-h		Prints this help message.
+	--help/-h       Prints this help message.
 
 Examples:
     # Starting a server that listens on `localhost:8080`
--- webserver.py
+++ webserver.py
@@ -75,26 +75,27 @@ def help():
 def read_args(args):
     server_args = ["localhost", 8080]
 
-    # If `--help` passed, prints help message and exits
-    if ("--help" in args) or ("-h" in args):
+    # Checks that correct number of arguments were passed
+    if len(args) == 0:
+        return server_args
+    elif (len(args) == 1) and ((args[0] == "--help") or (args[0] == "-h")):
         print(help())
         sys.exit(0)
-
-    # Checks that correct number of arguments were passed
-    if len(args) == 1:
-        return server_args
-    elif (len(args) % 2 == 0) or (len(args) > 5):
-        print(args[1])
+    elif (len(args) % 2 == 1) or (len(args) > 4):
+        if ("--help" in args) or ("-h" in args):
+            print(help())
+            sys.exit(0)
         err_msg = f"Invalid number of arguments: Recieved {len(args)}."
         raise ValueError(f"{err_msg}\n{help()}")
 
     # Checks that correct values were passed for arguments
-    while len(args) > 1:
-        option = args.pop(1)
-        arg = args.pop(1)
+    while len(args) > 0:
+        option = args.pop(0)
         if (option == "--hostname") or (option == "-n"):
+            arg = args.pop(0)
             server_args[0] = arg
         elif (option == "--port") or (option == "-p"):
+            arg = args.pop(0)
             try:
                 arg = int(arg)
             except ValueError:
@@ -105,8 +106,11 @@ def read_args(args):
                     " Must be in range 0-65535."
                 raise ValueError(f"{err_msg}\n{help()}")
             server_args[1] = arg
+        elif (option == "--help") or (option == "-h"):
+            print(help())
+            sys.exit(0)
         else:
-            err_msg = f"Invalid argument: '{arg}' is not a valid argument."
+            err_msg = f"Invalid option: '{option}' is not a valid option."
             raise ValueError(f"{err_msg}\n{help()}")
 
     return server_args
@@ -131,7 +135,8 @@ def run_server(hostname="localhost", port=8080):
 
 
 def main():
-    hostname, port = read_args(sys.argv)
+    # Excludes sys.argv[0] since it's just the name of this module
+    hostname, port = read_args(sys.argv[1:])
     run_server(hostname, port)
 
 
